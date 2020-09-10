ac84f482cb73abdee6126de0a136861c0ed0204f
Dave <dave@webdave.de>
2020-09-10 10:33:53 +0200

    some colorpicker magic booom

--- src/app/mouse-position/mouse-position.component.ts
+++ src/app/mouse-position/mouse-position.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { R3TargetBinder } from '@angular/compiler';
 
 @Component({
   selector: 'app-mouse-position',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class MousePositionComponent implements OnInit {
   x: number = 0;
   y: number = 0;
+  r: number = 50;
+  g: number = 50;
+  b: number = 50;
   constructor() {}
 
   ngOnInit(): void {}
@@ -17,5 +21,19 @@ export class MousePositionComponent implements OnInit {
 
     this.x = e.x;
     this.y = e.y;
+  }
+  range(e, c) {
+    console.log(e.target.value);
+    if (c === 'r') {
+      this.r = e.target.value;
+    } else if (c === 'g') {
+      this.g = e.target.value;
+    } else {
+      this.b = e.target.value;
+    }
+  }
+
+  getColor() {
+    return `rgb(${this.r},${this.g},${this.b})`;
   }
 }
