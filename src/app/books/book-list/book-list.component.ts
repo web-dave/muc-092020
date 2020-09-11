ee5809244395c6fe2231aed8785a35c454180269
Dave <dave@webdave.de>
2020-09-11 08:46:28 +0200

    observables

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,30 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IBook } from '../ibook';
+import { BookService } from '../book.service';
+import { interval, merge, Observable, Subscription } from 'rxjs';
+import { map, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styles: [],
 })
-export class BookListComponent implements OnInit {
-  books: IBook[] = [
-    {
-      title: 'Design Patterns',
-      isbn: 1,
-      subtitle: 'Elements of Reusable Object-Oriented Software',
-    },
-    {
-      title: 'REST und HTTP',
-      isbn: 2,
-      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web',
-    },
-    {
-      title: 'Eloquent JavaScript',
-      isbn: 3,
-      subtitle: 'A Modern Introduction to Programming',
-    },
-  ];
-  constructor() {}
+export class BookListComponent implements OnInit, OnDestroy {
+  book$: Observable<IBook[]>;
+  bar: Observable<IBook>;
+  foo: Observable<string>;
+  baz: Subscription;
+  a = interval(1500).pipe(map(() => 'affa'));
+  b = interval(500).pipe(map(() => 'baffe'));
+  c = interval(700).pipe(map(() => 'caffe'));
+  d = interval(2500).pipe(map(() => 'daffe'));
+  constructor(private service: BookService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.book$ = this.service
+      .getBooks()
+      .pipe(map((x) => x.filter((b) => b.isbn === 2)));
+    this.foo = merge<string>(this.a, this.b, this.c, this.d);
+    this.bar = this.service.getBooks2().pipe(
+      tap((data) => console.log(data)),
+      filter((b) => b.isbn === 1)
+    );
+
+    this.baz = this.service.getBooks2().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.baz.unsubscribe();
+  }
 }
--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { IBook } from './ibook';
+import { Observable, of, from } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private books: IBook[] = [
+    {
+      title: 'Design Patterns',
+      isbn: 1,
+      subtitle: 'Elements of Reusable Object-Oriented Software',
+    },
+    {
+      title: 'REST und HTTP',
+      isbn: 2,
+      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web',
+    },
+    {
+      title: 'Eloquent JavaScript',
+      isbn: 3,
+      subtitle: 'A Modern Introduction to Programming',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  getBooks(): Observable<IBook[]> {
+    return of(this.books);
+  }
+  getBooks2(): Observable<IBook> {
+    return from(this.books);
+  }
 }
